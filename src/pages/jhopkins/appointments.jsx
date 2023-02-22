7ea02e0af039e4fb18adcbb93c2f0449e4401a2b
Brian <bdlowery2@gmail.com>
2023-02-21 20:17:07 -0800

    Add sidebar.

--- src/pages/jhopkins/appointments.jsx
+++ src/pages/jhopkins/appointments.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import Sidebar from '@/components/sidebar'
-import { useUser } from '@auth0/nextjs-auth0/client';
+import React from 'react';
+import Sidebar from '@/components/sidebar';
+import {useUser} from '@auth0/nextjs-auth0/client';
 
 function appointments() {
-    const { user, error, isLoading } = useUser();
-    return (
-        <div className="flex">
-            <Sidebar />
-            <div className='bg-gray-100 w-full'>
-                <h1 className="text-2xl font-bold">Welcome back</h1>
-            </div>
-        </div>
-    )
+   // const { user, error, isLoading } = useUser();
+   return (
+      <div className="flex" id="site-content">
+         <Sidebar />
+         <div className="bg-gray-100 w-full">
+            <h1 className="text-2xl font-bold">Welcome back</h1>
+         </div>
+      </div>
+   );
 }
 
-export default appointments
+export default appointments;
