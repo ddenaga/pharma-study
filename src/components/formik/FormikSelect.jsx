4f471c6b4a9a314987224f7aad616992df4dfbd8
Brian <bdlowery2@gmail.com>
2023-03-09 15:56:45 -0700

    …

--- src/components/formik/FormikSelect.jsx
+++ src/components/formik/FormikSelect.jsx
@@ -7,7 +7,7 @@ const FormikSelect = ({ label, className, ...props }) => {
 		<>
 			<label htmlFor={props.id || props.name}>{label}</label>
 			<select
-				className="mt-2 block w-full rounded-md border-0 bg-white bg-[right_0.5rem_center] py-1.5 px-3 pr-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+				className="mt-2 block w-full rounded-md border-0 bg-white bg-[right_0.5rem_center] py-1.5 px-3 pr-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 focus-visible:ring-0 sm:text-sm sm:leading-6"
 				{...field}
 				{...props}
 			/>
