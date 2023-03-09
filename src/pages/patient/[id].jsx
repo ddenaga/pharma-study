8cee991d342a84248320c68ecfe42c4ec2651184
victor severin <vikea.severin@gmail.com>
2023-03-08 22:14:26 -0800

    picture card + blood info card

--- src/pages/patient/[id].jsx
+++ src/pages/patient/[id].jsx
@@ -23,81 +23,100 @@ export default function Patient(props) {
 	return (
 		<div className="flex" id="site-content">
 			<Sidebar />
-			<div className="bg-gray-100 w-full flex flex-col">
+			<div className="bg-gray-100 w-full grid grid-cols-12 content-start gap-6">
 				{/* View content goes here */}
-				<button onClick={() => console.log(patient)}>Click me</button>
+				{/* <button onClick={() => console.log(patient)}>Click me</button> */}
 
-				
-
-
-				<div className='flex border bg-white p-4 rounded-xl m-9 max-w-fit gap-32'>
-					<div className='flex flex-col justify-between'>
+				<div className='flex border bg-white justify-around p-4 mt-8 ml-8 rounded-xl gap-32 col-span-5 row-span-3'>
+					<div className='flex flex-col items-center'>
 						<Image
 							src="/../public/favicon.ico"
-							width="80"
-							height="80"
+							width="150"
+							height="150"
 							alt="patient avatar"
-							/>
-						<span className='font-bold text-2xl'>{patient.name}</span>
-						<div className='flex flex-col'>
-							<span className='font-semibold text-lg'>DOB</span>
-							<span>{patient.dob}</span>
+						/>
+						<span className='font-bold text-5xl mt-2 text-slate-600'>{patient.name}</span>
+						<div className='flex flex-col pt-10 content-start'>
+							<span className='font-semibold text-3xl text-slate-600'>DOB</span>
+							<span className='text-xl'>{patient.dob}</span>
 						</div>
 					</div>
-					<div className='flex flex-col'>
-						<span className='font-semibold text-lg'>Address</span>
-						<span>{patient.address.streetAddress + ', ' + patient.address.city}</span>
-						<span>{patient.address.state + ', ' + patient.address.zipCode}</span>
-						<span className='font-semibold text-lg'>Insurance #</span>
-						<span>{patient.insuranceNumber}</span>
-						<span className='font-semibold text-lg'>Employed</span>
-						<span>{patient.isEmployed ? "Yes" : "No"}</span>
+					<div className='flex flex-col justify-around'>
+						<div className='flex flex-col'>
+							<span className='font-semibold text-3xl text-slate-600'>Address</span>
+							<span className='text-lg'>{patient.address.streetAddress + ', ' + patient.address.city}</span>
+							<span className='text-lg'>{patient.address.state + ', ' + patient.address.zipCode}</span>
+						</div>
+						<div className='flex flex-col'>
+							<span className='font-semibold text-3xl text-slate-600'>Insurance #</span>
+							<span className='text-xl'>{patient.insuranceNumber}</span>
+						</div>
+						<div className='flex flex-col'>
+							<span className='font-semibold text-3xl text-slate-600'>Employed</span>
+							<span className='text-xl'>{patient.isEmployed ? "Yes" : "No"}</span>
+						</div>
 					</div>
 				</div>
 
 				{/* Height, Weight, Blood Type, Blood Pressure, Oxygen Saturation */}
 
-				<div className="flex w-96 h-30 drop-shadow-sm border-2 m-9 bg-white rounded-xl justify-between items-center p-4">
-					<ul className='flex flex-col'>
-						<li>
+				<div className="flex flex-col drop-shadow-sm border-2 bg-white rounded-xl items-center p-4 col-span-2 justify-center mt-8">
+					<div className='flex w-full  items-center'>
+						<FontAwesomeIcon className='text-red-500 text-3xl w-14' icon={faDroplet} />
+						<div className='flex flex-col items-center'>
+							<span className=' text-md'>Blood Type</span>
+							<span className="text-red-500 text-2xl">{patient.bloodType}</span>
+						</div>
+					</div>
+
+					<div className='flex w-full  items-center'>
+						<FontAwesomeIcon icon={faGauge} className="text-3xl w-14" />
+						<div className='flex flex-col items-center'>
+							<span className=' text-md'>Blood Pressure</span>
+							<span className="text-red-500 text-2xl">{patient.bloodPressure}</span>
+						</div>
+					</div>
+					<div className='flex w-full items-center'>
+						<FontAwesomeIcon className='text-blue-500 text-3xl w-14 justify-center' icon={faWind} />
+						<div className='flex flex-col items-center'>
+							<span className=' text-md'>Oxygen Saturation</span>
+							<span className="text-red-500 text-2xl">{patient.oxygenSaturation}</span>
+						</div>
+					</div>
+					<div className='flex w-full items-center'>
+						<FontAwesomeIcon className='text-red-500 text-3xl w-14' icon={faTemperatureHalf} />
+						<div className='flex flex-col items-center'>
+							<span className=' text-md'>Temperature</span>
+							<span className="text-red-500 text-2xl">{patient.temperature}</span>
+						</div>
+					</div>
+				</div>
+				<div className="flex drop-shadow-sm border-2 bg-white rounded-xl justify-between items-center p-4 col-span-2 ">
+					<div>
+						<div>
 							<span>Height</span>
 							{patient.height}
-						</li>
-						<li>
+						</div>
+						<div>
 							<span>Weight</span>
 							{patient.weight}
-						</li>
-						<li>
-							<FontAwesomeIcon className='text-red-500' icon={faTemperatureHalf} />
-							<span>Temperature</span>
-							{patient.temperature}
-						</li>
-						<li>
-							<FontAwesomeIcon className='text-red-500' icon={faDroplet} />
-							<span>Blood Type</span>
-							{patient.bloodType}
-						</li>
-						<li>
-							<FontAwesomeIcon icon={faGauge} />
-							<span>Blood Pressure</span>
-							{patient.bloodPressure}
-						</li>
-						<li>
-							<FontAwesomeIcon className='text-blue-500' icon={faWind} />
-							<span>Oxygen Saturation</span>
-							{patient.oxygenSaturation}
-						</li>
-					</ul>
+						</div>
+						<div>
+							<span>BMI</span>
+							<span>26.5</span>
+						</div>
+
+					</div>
 				</div>
 
 				{/* Visits */}
-				<div className="flex w-96 h-30 drop-shadow-sm border-2 m-9 bg-white rounded-xl justify-between items-center p-4">
+				<div className="flex drop-shadow-sm border-2 bg-white rounded-xl justify-between items-center p-4 col-span-5 row-span-2">
 					Visits
 				</div>
 
 				{/*  */}
-				<div className="flex w-96 h-30 drop-shadow-sm border-2 m-9 bg-white rounded-xl justify-between items-center p-4">
-					
+				<div className="flex drop-shadow-sm border-2 bg-white rounded-xl justify-between items-center p-4 col-span-7">
+
 				</div>
 			</div>
 		</div>
