984c6810699fbbd89e7fa0d94acd3f82e225b204
Brian <bdlowery2@gmail.com>
2023-03-09 15:58:39 -0700

    update label styles

--- src/components/formik/FormikList.jsx
+++ src/components/formik/FormikList.jsx
@@ -14,7 +14,9 @@ const FormikList = ({ label, className, ...props }) => {
 				const { [name]: vals } = values;
 				return (
 					<>
-						<label htmlFor={name}>{label}</label>
+						<label htmlFor={name} className="block text-sm font-medium leading-6 text-gray-900">
+							{label}
+						</label>
 						<input
 							className="mt-2 block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 focus-visible:ring-0 sm:text-sm sm:leading-6"
 							value={value}
--- src/components/formik/FormikSelect.jsx
+++ src/components/formik/FormikSelect.jsx
@@ -5,7 +5,9 @@ const FormikSelect = ({ label, className, ...props }) => {
 
 	return (
 		<>
-			<label htmlFor={props.id || props.name}>{label}</label>
+			<label htmlFor={props.id || props.name} className="block text-sm font-medium leading-6 text-gray-900">
+				{label}
+			</label>
 			<select
 				className="mt-2 block w-full rounded-md border-0 bg-white bg-[right_0.5rem_center] py-1.5 px-3 pr-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 focus-visible:ring-0 sm:text-sm sm:leading-6"
 				{...field}
