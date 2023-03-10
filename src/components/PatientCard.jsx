8808309207ecefc02f79c3ab5e42d8af4b655242
ddenaga <76412992+ddenaga@users.noreply.github.com>
2023-03-09 16:57:07 -0800

    Add patient images

--- src/components/PatientCard.jsx
+++ src/components/PatientCard.jsx
@@ -1,23 +1,19 @@
 import React from "react";
 import Image from "next/image";
-export default function PatientCard({
-    name,
-    dob,
-    familyHistory,
-    img,
-    id,
-    eligibility,
-}) {
+export default function PatientCard(props) {
+
+	const { patient } = props;
+
     return (
         <div className="flex w-96 h-30 drop-shadow-sm border-2 m-9 bg-white rounded-xl justify-between items-center p-4">
             <div>
                 <div className="flex flex-col">
-                    <span className="text-2xl">{name}</span>
-                    <span className="text-gray-400">{dob}</span>
-                    <span className="text-gray-400">{familyHistory}</span>
+                    <span className="text-2xl">{patient.name}</span>
+                    <span className="text-gray-400">{patient.dob}</span>
+                    <span className="text-gray-400">{patient.familyHistory}</span>
                 </div>
                 <div>
-                    <a href={`/patient/${id}`} className="mt-5">
+                    <a href={`/patient/${patient._id}`} className="mt-5">
                         More Info
                     </a>
                 </div>
@@ -25,12 +21,12 @@ export default function PatientCard({
             {/* TODO change eligibility to icons */}
             <div className="flex flex-col justify-between h-full ">
                 <Image
-                    src="/../public/favicon.ico"
+                    src={patient.pictureUrl == "" ? "/../public/favicon.ico" : patient.pictureUrl}
                     width="50"
                     height="50"
                     alt="patient avatar"
                 />
-                {eligibility ? <span>Eligible</span> : <span>Not Eligible</span>}
+                {patient.isEligible ? <span>Eligible</span> : <span>Not Eligible</span>}
             </div>
         </div>
     );
--- src/components/PatientList.jsx
+++ src/components/PatientList.jsx
@@ -1,29 +1,20 @@
-import React from "react";
-import Patient from "@/components/PatientCard";
-export default function PatientList({ patients, searchInput }) {
-  const filteredData = patients.filter((patient) => {
-    if (searchInput === "") {
-      return patient;
-    } else {
-      return patient.name.toLowerCase().includes(searchInput);
-    }
-  });
+import React from 'react';
+import PatientCard from '@/components/PatientCard';
 
-  return (
-    <div
-      className="flex flex-wrap justify-between bg-scroll"
-      onClick={console.log(patients)}
-    >
-      {filteredData.map((patient) => (
-        <Patient
-          key={patient._id}
-          name={patient.name}
-          dob={patient.dob}
-          familyHistory={patient.familyHistory}
-          id={patient._id}
-          eligibility={patient.isEligible}
-        />
-      ))}
-    </div>
-  );
+export default function PatientList({ patients, searchInput }) {
+	const filteredData = patients.filter((patient) => {
+		if (searchInput === '') {
+			return patient;
+		} else {
+			return patient.name.toLowerCase().includes(searchInput);
+		}
+	});
+
+	return (
+		<div className="flex flex-wrap justify-between bg-scroll" onClick={() => console.log(patients)}>
+			{filteredData.map((patient) => (
+				<PatientCard patient={patient} />
+			))}
+		</div>
+	);
 }
