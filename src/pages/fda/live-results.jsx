e07515afcd6b263b012a0592ff999a321b07400d
victor severin <vikea.severin@gmail.com>
2023-03-11 01:10:42 -0800

    setup websocket

--- src/pages/fda/live-results.jsx
+++ src/pages/fda/live-results.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Sidebar from '@/components/Sidebar';
+import { jhClient } from '@/lib/vendia';
 
 function LiveResults() {
+	useEffect(() => {
+		const res = jhClient.entities.patient.onUpdate((data) => {
+			console.log(data.result)
+		});
+		}, [])
 	return (
 		<div className="flex" id="site-content">
 			<Sidebar />
