4a7042343f47f412ac2459f658f100cf9e78cade
Matt Christiansen <mattchristiansen59@gmail.com>
2023-03-13 13:32:01 -0700

    initial setup and test enivronment, along with first test

---
+++ src/components/__tests__/get-patient-list-fda.test.js
@@ -0,0 +1,7 @@
+import { render, screen, cleanup } from '@testing-library/react';
+
+//import AssignDrugs from '../../pages/fda/assign-drugs.jsx';
+
+test('should not see PII and instead see nulls', () => {
+    expect(true).toBe(true);
+})
