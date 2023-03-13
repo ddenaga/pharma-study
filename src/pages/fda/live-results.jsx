7d6b388fc07edc383e12c8be86e3d1b67fee8a95
ddenaga <76412992+ddenaga@users.noreply.github.com>
2023-03-13 15:12:06 -0700

    Implement mappings for FDA

--- src/pages/fda/live-results.jsx
+++ src/pages/fda/live-results.jsx
@@ -1,11 +1,46 @@
 import React from 'react';
 import Sidebar from '@/components/Sidebar';
+import { fdaClient } from '@/lib/vendia';
 
-function LiveResults() {
+export async function getServerSideProps() {
+	const data = await fdaClient.entities.tracker.list();
+	const mappings = data.items;
+
+	// Note: This could be intensive on the API.
+	const entities = await Promise.all(
+		mappings.map(async (ids) => {
+			const { _id, patientId, treatmentId } = ids;
+			const patient = await fdaClient.entities.patient.get(patientId);
+			const treatment = await fdaClient.entities.treatment.get(treatmentId);
+
+			return {
+				_id,
+				patient,
+				treatment,
+			};
+		}),
+	);
+
+	return {
+		props: {
+			entities,
+		},
+	};
+}
+
+function LiveResults(props) {
 	return (
 		<div className="flex" id="site-content">
 			<Sidebar />
-			<div className="bg-gray-100 w-full">{/* View content goes here */}</div>
+			<div className="w-full bg-gray-100">
+				<button
+					onClick={() => {
+						console.log(props.entities);
+					}}
+				>
+					Click me
+				</button>
+			</div>
 		</div>
 	);
 }
