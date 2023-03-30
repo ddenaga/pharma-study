978e142e1d785322fa8ae9e360e66cd886e9520d
Brian <bdlowery2@gmail.com>
2023-03-30 17:41:08 -0600

    Edit all patients and patient detail page css.

--- src/pages/jhopkins/admin/all-patients.jsx
+++ src/pages/jhopkins/admin/all-patients.jsx
@@ -1,42 +1,42 @@
-import React from "react";
-import Sidebar from "@/components/Sidebar";
-import { jhClient } from "@/lib/vendia.js";
-import PatientCardAdmin from "@/components/patientCardAdmin";
-import { useUser } from "@auth0/nextjs-auth0/client";
+import React from 'react';
+import Sidebar from '@/components/Sidebar';
+import { jhClient } from '@/lib/vendia.js';
+import PatientCardAdmin from '@/components/patientCardAdmin';
+import { useUser } from '@auth0/nextjs-auth0/client';
 
 export default function AllPatients(props) {
-  const { user, error, isLoading } = useUser();
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>{error.message}</div>;
-  return (
-    <div className="flex" id="site-content">
-      <Sidebar />
-      <div className="bg-gray-100 w-full">
-        <div className="flex mt-16 justify-between">
-          <h1 className="text-4xl font-bold ml-8 text-zinc-600">My Patients</h1>
-        </div>
-        <div className="flex flex-wrap justify-between">
-          {props.data.items.map((patient) => (
-            <PatientCardAdmin
-              key={patient._id}
-              name={patient.name}
-              dob={patient.dob}
-              familyHistory={patient.familyHistory}
-              id={patient._id}
-              eligibility={patient.isEligible}
-            />
-          ))}
-        </div>
-      </div>
-    </div>
-  );
+	const { user, error, isLoading } = useUser();
+	if (isLoading) return <div>Loading...</div>;
+	if (error) return <div>{error.message}</div>;
+	return (
+		<div className="flex" id="site-content">
+			<Sidebar />
+			<div className="w-full overflow-y-scroll bg-gray-50 px-20 py-12">
+				<div className="">
+					<h1 className="attention-voice mb-12">My Patients</h1>
+				</div>
+				<div className="grid grid-cols-1 gap-8 lg:grid-cols-2 xl:grid-cols-3">
+					{props.data.items.map((patient) => (
+						<PatientCardAdmin
+							key={patient._id}
+							name={patient.name}
+							dob={patient.dob}
+							familyHistory={patient.familyHistory}
+							id={patient._id}
+							eligibility={patient.isEligible}
+						/>
+					))}
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export async function getServerSideProps() {
-  const myData = await jhClient.entities.patient.list();
-  return {
-    props: {
-      data: myData,
-    },
-  };
+	const myData = await jhClient.entities.patient.list();
+	return {
+		props: {
+			data: myData,
+		},
+	};
 }
