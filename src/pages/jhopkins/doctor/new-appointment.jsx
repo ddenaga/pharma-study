8ac5ff4ab32dbb1cf58ff08a1ad14b733c78308b
ddenaga <76412992+ddenaga@users.noreply.github.com>
2023-04-12 21:38:34 -0700

    Fix jsx attribute and useEffect errors

--- src/pages/jhopkins/doctor/new-appointment.jsx
+++ src/pages/jhopkins/doctor/new-appointment.jsx
@@ -5,6 +5,7 @@ import { jhClient } from '@/lib/vendia.js';
 import FormikDatePicker from 'src/components/formik/FormikDatePicker';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
+import FormikSelect from '@/components/formik/FormikSelect';
 
 export async function getServerSideProps() {
 	const data = await jhClient.entities.patient.list();
@@ -41,39 +42,16 @@ export default function ScheduleAppointment(props) {
 	};
 
 	const handleSubmit = (values, { setSubmitting }) => {
+		const { appointmentDate, appointmentTime, patientId } = values;
+		// Guard clause to ensure all appointment details have been selected
+		if (!appointmentDate || !appointmentTime || !patientId) return;
+
+		const updateAppointments = [...appointments, { ...values, id: new Date().getTime() }];
 		// Add a new appointment to the list with a unique id
-		setAppointments([...appointments, { ...values, id: new Date().getTime() }]);
+		setAppointments(updateAppointments);
 
 		// Reset the form and setSubmitting to false
 		setSubmitting(false);
-	};
-
-	useEffect(() => {
-		setAppointments((prevAppointments) =>
-			[...prevAppointments].sort((a, b) => {
-				const dateA = new Date(a.appointmentDate);
-				const dateB = new Date(b.appointmentDate);
-				const timeA = a.appointmentTime;
-				const timeB = b.appointmentTime;
-
-				if (dateA < dateB) {
-					return -1;
-				} else if (dateA > dateB) {
-					return 1;
-				} else {
-					// Both dates are the same; compare times
-					return timeA.localeCompare(timeB);
-				}
-			}),
-		);
-	}, [appointments]);
-
-	const onDateChange = (value) => {
-		setSelectedDate(value);
-	};
-
-	const onTimeClick = (time) => {
-		setSelectedTime(time);
 	};
 
 	return (
@@ -81,18 +59,10 @@ export default function ScheduleAppointment(props) {
 			<Sidebar />
 			<div className="flex-1 bg-gray-100 p-8">
 				<h1 className="title mb-9 text-3xl font-bold">Schedule New Appointment</h1>
-				<button
-					className="btn"
-					onClick={() => {
-						console.log(appointments);
-					}}
-				>
-					Click me
-				</button>
 
 				<Formik
 					initialValues={{
-						patientId: null,
+						patientId: '',
 						appointmentDate: new Date(),
 						appointmentTime: '',
 					}}
@@ -104,14 +74,14 @@ export default function ScheduleAppointment(props) {
 								<label htmlFor="patientId" className="mb-1">
 									Select Patient:
 								</label>
-								<Field as="select" name="patientId" className="border p-2">
+								<FormikSelect name="patientId">
 									<option value="">Select a patient</option>
-									{patients.map((patient) => (
-										<option key={patient.id} value={patient._id}>
+									{patients.map((patient, index) => (
+										<option key={index} value={patient._id}>
 											{patient.name}
 										</option>
 									))}
-								</Field>
+								</FormikSelect>
 							</div>
 							<div className="flex flex-col md:flex-row md:space-x-4">
 								<div className="rounded border p-2">
@@ -130,7 +100,7 @@ export default function ScheduleAppointment(props) {
 													<Calendar
 														value={selectedDate}
 														onChange={(value) => {
-															onDateChange(value);
+															setSelectedDate(value);
 															setFieldValue('appointmentDate', value);
 														}}
 													/>
@@ -182,12 +152,12 @@ export default function ScheduleAppointment(props) {
 													return timeA.localeCompare(timeB);
 												}
 											})
-											.map((appointment) => {
+											.map((appointment, index) => {
 												const patient = getPatientById(appointment.patientId);
 
 												return (
 													<li
-														key={appointment.id}
+														key={index}
 														className="flex items-center justify-between border-b py-2"
 													>
 														<span>
@@ -198,6 +168,7 @@ export default function ScheduleAppointment(props) {
 															{appointment.appointmentTime}
 														</span>
 														<button
+															type="button"
 															onClick={() => deleteAppointment(appointment.id)}
 															className="rounded bg-red-500 py-1 px-3 text-white"
 														>
@@ -209,66 +180,63 @@ export default function ScheduleAppointment(props) {
 									</ul>
 								</div>
 							</div>
-							<button
-								type="submit"
-								disabled={isSubmitting}
-								className="mt-4 rounded bg-blue-500 py-2 px-4 text-white"
-							>
+							<button type="submit" className="mt-4 rounded bg-blue-500 py-2 px-4 text-white">
 								Create New Appointment
+							</button>
+							<button
+								type="button"
+								className="btn"
+								onClick={() => {
+									appointments.forEach(async (appt) => {
+										const patient = getPatientById(appt.patientId);
+
+										// Combine appointmentDate and appointmentTime into
+										// appointmentDate is a Date object
+										// appointmentTime is a time string
+
+										// Source from: https://stackoverflow.com/a/40197728
+										const convertTime12to24 = (time12h) => {
+											const [time, modifier] = time12h.split(' ');
+
+											let [hours, minutes] = time.split(':');
+
+											if (hours === '12') {
+												hours = '00';
+											}
+
+											if (modifier === 'PM') {
+												hours = parseInt(hours, 10) + 12;
+											}
+
+											return `${hours}:${minutes}`;
+										};
+
+										const scheduledDateTime = `${
+											appt.appointmentDate.toISOString().split('T')[0]
+										}T${convertTime12to24(appt.appointmentTime)}`;
+
+										const visit = {
+											dateTime: new Date(scheduledDateTime).toISOString(),
+											note: '',
+											hivViralLoad: '',
+										};
+
+										patient.visits = [...patient.visits, visit];
+
+										// Update the patient's visit in Vendia
+										const updatePatient = (({ _id, visits }) => ({ _id, visits }))(patient);
+										const res = await jhClient.entities.patient.update(updatePatient);
+										console.log(res);
+									});
+
+									setAppointments([]);
+								}}
+							>
+								Submit
 							</button>
 						</Form>
 					)}
 				</Formik>
-				<button
-					className="btn"
-					onClick={() => {
-						appointments.forEach(async (appt) => {
-							const patient = getPatientById(appt.patientId);
-
-							// Combine appointmentDate and appointmentTime into
-							// appointmentDate is a Date object
-							// appointmentTime is a time string
-
-							// Source from: https://stackoverflow.com/a/40197728
-							const convertTime12to24 = (time12h) => {
-								const [time, modifier] = time12h.split(' ');
-
-								let [hours, minutes] = time.split(':');
-
-								if (hours === '12') {
-									hours = '00';
-								}
-
-								if (modifier === 'PM') {
-									hours = parseInt(hours, 10) + 12;
-								}
-
-								return `${hours}:${minutes}`;
-							};
-
-							const scheduledDateTime = `${
-								appt.appointmentDate.toISOString().split('T')[0]
-							}T${convertTime12to24(appt.appointmentTime)}`;
-
-							const visit = {
-								dateTime: new Date(scheduledDateTime).toISOString(),
-								note: '',
-								hivViralLoad: '',
-							};
-
-							patient.visits = [...patient.visits, visit];
-
-							// Update the patient's visit in Vendia
-							const updatePatient = (({ _id, visits }) => ({ _id, visits }))(patient);
-							const res = await jhClient.entities.patient.update(updatePatient);
-							console.log(res);
-						});
-
-						setAppointments([]);
-					}}
-				>
-					Submit
-				</button>
 			</div>
 		</div>
 	);
