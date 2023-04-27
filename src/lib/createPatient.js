08be219d224b2e393ef214cd5d200ff4d8b77ed0
ddenaga <76412992+ddenaga@users.noreply.github.com>
2023-04-27 15:17:29 -0700

    Fix eligibility check
    Co-authored-by: Matt Christiansen <pythagoras-19@users.noreply.github.com>

--- src/lib/createPatient.js
+++ src/lib/createPatient.js
@@ -10,18 +10,21 @@ export default async function createPatient(data) {
   // Determine Patient eligibility
   let isEligible = true;
 
+
+  // No pregnancy health codes (starting with O)
+  isEligible = data.icdHealthCodes?.every((code) => code.toUpperCase()[0] !== 'O');
+
+  // No DOB later than 2005
   let dob = new Date(data.dob);
   let deadLine = new Date("01-01-2005");
   if (dob > deadLine) isEligible = false;
 
-  for (const icd in data.icdHealthCodes) {
-    if (icd.toUpperCase()[0] == "O") {
-      isEligible = false;
-      break;
-    }
-  }
 
   data.isEligible = isEligible;
+  console.log(isEligible);
+
+  data.isEligible = isEligible;
+
 
   // Add the patient
   const addResponse = await jhClient.entities.patient.add(data, patientAcl);
