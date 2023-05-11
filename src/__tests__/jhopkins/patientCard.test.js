262df69729ec7daec5b186fe44143fbf312204b3
victor severin <vikea.severin@gmail.com>
2023-05-11 09:39:05 -0700

    patientCard test

--- src/__tests__/jhopkins/patientCard.test.js
+++ src/__tests__/jhopkins/patientCard.test.js
@@ -1,14 +1,11 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import PatientCardAdmin from '@/components/PatientCardAdmin';
 import '@testing-library/jest-dom/extend-expect';
 
-// test('renders patient card', () => {
-//     render(<PatientCardAdmin />);
-// });
 
 test('renders patient information correctly', () => {
     const name = 'John Doe';
-    const dob = '1990-01-01T00:00:00Z';
+    const dob = '1990-01-01';
     const familyHistory = 'No family history';
     const img = '/../public/favicon.ico';
     const eligibility = true;
@@ -24,8 +21,8 @@ test('renders patient information correctly', () => {
             eligibility={eligibility}
         />
     );
+
     expect(screen.getByText(name)).toBeInTheDocument();
-    expect(screen.getByText(dob.split('T')[0])).toBeInTheDocument();
+    expect(screen.getByText(dob)).toBeInTheDocument();
     expect(screen.getByText(familyHistory)).toBeInTheDocument();
-    // Assert the presence and correctness of the patient information in the rendered output
 });
